73137dd1c45262eeab0f4b0ba74f3157a53cb80d
Gigil Jacob <gigiljacob93@gmail.com>
2021-10-20 17:39:31 +0530

    Testimonial changed

---
+++ Abytech/assets/js/mail.js
@@ -0,0 +1,13 @@
+
+$('.php-email-form').on('submit', function () {
+    let toEmail = "gigiljacob93@gmail.com"
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let subject = document.getElementById('subject').value;
+    let message = document.getElementById('message').value;
+
+    
+
+    return true;
+});
